64e4f95f977290492ce828656b90ebb48becbc62
jflipp31698 <48108288+jflipp31698@users.noreply.github.com>
2021-03-13 21:42:46 -0600

    Card styles changed

--- src/components/Post.js
+++ src/components/Post.js
@@ -9,9 +9,9 @@ const Post = ({post}) => {
                     <a className="postLink" target="_blank" rel="noreferrer" href={'https://' + post.id}>Post Link</a>
                     <div className="likesDiv">
                         <h2 className="postLikes">{post.likes}</h2>
-                        <FaHeart color="#ff6961" size="5%"/>
+                        <FaHeart color="#ff6961" size="7%"/>
                     </div>
-                    <h2 className="postScore">{post.viralScore}</h2>
+                    <h2 className="postScore">{post.viralScore + "%"}</h2>
                 <img className="postIMG" alt={post.id} src={post.imgURL} width="500" height="500"/>
             </div>
         </center>
